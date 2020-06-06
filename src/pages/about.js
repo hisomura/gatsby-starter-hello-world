f6c5781ac66c6dfbdb01096fdcfc4709e7188b31
hisomura <hisomura.noreply@gmail.com>
2020-06-06 20:31:10 +0900

    Step1 done.

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../../components/header"
+
+export default function About() {
+  return (
+    <div style={{ color: `teal` }}>
+      <Header headerText="About Gatsby" />
+      <h1>About Gatsby</h1>
+      <Link to="/">Home</Link>
+      <p>Such wow. Very React.</p>
+    </div>
+  )
+}
