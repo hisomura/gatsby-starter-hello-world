06a728d47df842ece5c9221ee592120160def835
hisomura <hisomura.noreply@gmail.com>
2020-06-08 10:21:40 +0900

    Step2 done.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../../components/header"
+import Header from "../components/header"
+import Container from "../components/container"
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <Link to='/contact'>Contact</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/about-css-modules'>About CSS Modules</Link>
       <Header headerText="Hello Gatsby!"/>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
