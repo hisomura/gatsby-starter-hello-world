915c79cf194a26ee8d982b234a0a603ab8f4a3ab
hisomura <hisomura.noreply@gmail.com>
2020-08-27 08:01:03 +0900

    Step8 done.

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,9 @@
 
 module.exports = {
   siteMetadata: {
-    title: `Title from siteMetadata`,
+    title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
   },
   /* Your site config here */
   plugins: [
@@ -19,11 +21,27 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
+    // {
+    //   resolve: `gatsby-plugin-typography`,
+    //   options: {
+    //     pathToConfigModule: `src/utils/typography`,
+    //   },
+    // },
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: `gatsby-plugin-manifest`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
   ],
 }
